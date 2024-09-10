d02d3746572f49708544c2d7fc36c3077894b56e
jbroach <joebroach@gmail.com>
2024-09-09 22:44:10 -0700

    create new file for r_basics

---
+++ R/r_basics.R
@@ -0,0 +1,23 @@
+#### R Basics ####
+# "A foolish consistency is the hobgoblin of 
+#   little minds"   -Ralph Waldo Emerson 
+
+# Literals ----
+
+# Operators ----
+
+# variables ----
+
+
+# data structures ----
+# vectors have a single dimension, like a column or row of data
+
+# data frames - the key structure for data science, multi-dimensional
+#   collections of vectors
+
+
+# Special type: factors, and putting it all together ----
+# factors are categorical variables with a fixed set of
+#   potential values
+
+
