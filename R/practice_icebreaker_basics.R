26f468cad0eafe0c6f50910384965c0b06af565f
jbroach <joebroach@gmail.com>
2024-09-09 23:28:47 -0700

    created practice problem using icebreaker dataset

---
+++ R/practice_icebreaker_basics.R
@@ -0,0 +1,27 @@
+#### Practice Problem: Loading and manipulating a data frame ####
+# Don't forget: Comment anywhere the code isn't obvious to you!
+
+# Load the readxl and dplyr packages
+
+# Use the read_excel function to load the class survey data
+
+# Take a peek!
+
+# Create a travel_speed column in your data frame using vector operations and 
+#   assignment
+
+# Look at a summary of the new variable--seem reasonable?
+
+# Choose a travel mode, and use a pipe to filter the data by your travel mode
+
+# Note the frequency of the mode (# of rows returned)
+
+# Repeat the above, but this time assign the result to a new data frame
+
+# Look at a summary of the speed variable for just your travel mode--seem 
+#   reasonable?
+
+# Filter the data by some arbitrary time, distance, or speed threshold
+
+# Stretch yourself: Repeat the above, but this time filter the data by two 
+#   travel modes (Hint: %in%)
