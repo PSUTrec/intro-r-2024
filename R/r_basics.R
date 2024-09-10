3587fed52ebe2d1787e2ed650962e35506fed75b
jbroach <joebroach@gmail.com>
2024-09-10 14:37:38 -0700

    intro to literals

--- R/r_basics.R
+++ R/r_basics.R
@@ -3,6 +3,12 @@
 #   little minds"   -Ralph Waldo Emerson 
 
 # Literals ----
+"this is a string literal"  # double quotes preferred in R but not required
+42
+T
+F
+TRUE
+FALSE
 
 # Operators ----
 
