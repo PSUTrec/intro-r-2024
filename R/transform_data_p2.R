128e61dd1d3c0e3d390fa24a5f7eb4e7f444031c
Joe Broach <joebroach@gmail.com>
2024-09-11 08:59:18 -0700

    created transformation pt 2 shell

---
+++ R/transform_data_p2.R
@@ -0,0 +1,10 @@
+#### Transforming Data Part 2 ####
+
+# load libraries (by convention done at top of scripts)
+
+# Distinct & Select (reducing data rows or columns) ----
+
+
+# mutate and rename (modifying data frame in place) ----
+
+
